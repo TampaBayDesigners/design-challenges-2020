39b513cf13078e8bc97a773e771f0b34e5e05200
Ryan Parag <parag.ryan@gmail.com>
2020-03-15 16:10:47 -0400

    hover and content fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,10 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Section, Wrapper, Grid, Col } from "../components/Grid"
+import { CardStatic } from "../components/Card"
+import { Link } from 'gatsby'
+import { theme } from "../theme/theme"
 
 class NotFoundPage extends React.Component {
   render() {
@@ -12,13 +16,19 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <div style={{
-          textAlign: `center`,
-          padding: `3.2rem 0`,
-        }}>
-          <h3>Not Found</h3>
-          <p>It seems this design challenge does not exist ... yet</p>
-        </div>
+        <Section>
+          <Wrapper>
+            <Grid>
+              <Col>
+                <CardStatic style={{ textAlign: 'center' }}>
+                  <h2>Not Found</h2>
+                  <p>It seems this page does not exist ... yet</p>
+                  <Link to={'/'} style={{ textDecoration: 'none', color: theme.orange }}>Back Home</Link>
+                </CardStatic>
+              </Col>
+            </Grid>
+          </Wrapper>
+        </Section>
       </Layout>
     )
   }
